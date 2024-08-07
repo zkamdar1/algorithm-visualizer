3dec5a5b9d1e066effa050bb4365a21a72c60ed0
zkamdar <zkamdar0908@gmail.com>
2024-08-07 17:20:17 -0400

    added basic sorting visuals

--- algo-visuals/src/App.js
+++ algo-visuals/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+
+
 
 function App() {
   return (
     <div className="App">
       <PathfindingVisualizer></PathfindingVisualizer>
+      <SortingVisualizer></SortingVisualizer>
     </div>
   );
 }
